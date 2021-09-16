f5f04899b465da181c12fe0f31041e724d372c26
AndoreiCR <dexter_andrey@hotmail.com>
2021-09-16 16:16:21 -0600

    Create World.java

---
+++ World.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+
+class HelloWorld {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hello World");
+        JOptionPane.showMessageDialog(null, "Esto es una prueba de commit");
+    }
+}
