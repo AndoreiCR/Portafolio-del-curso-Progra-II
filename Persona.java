9ca0ee326383b01d15eeafc127809d034f00be08
AndoreiCR <dexter_andrey@hotmail.com>
2021-09-22 19:53:43 -0600

    Cambios
    Cambios en persona con gets y sets

--- Persona.java
+++ Persona.java
@@ -1,14 +1,50 @@
 public class Persona {
-    int id = 0;
+    private int id = 0;
     String nombre = "";
     String apellido = "";
     short edad = 0;
 
-    void saludar() {
-        System.out.println("Hola me llamo " + nombre);
+    public int getId() {
+        return id;
     }
 
-    String imprimir(){
-        return "Soy "+nombre;
+    public void setId(int id) {
+        this.id = id;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public short getEdad() {
+        return edad;
+    }
+
+    public void setEdad(short edad) {
+        this.edad = edad;
+    }
+
+
+    void saludar() {
+        System.out.println("Hola me llamo " + nombre
+        + " Y mi ID es "+ id);
+    }
+
+    String imprimir() {
+        return "Soy " + nombre;
+    }
+
+    
 }
--- World.java
+++ World.java
@@ -4,13 +4,14 @@ import javax.swing.JPopupMenu;
 class HelloWorld {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,"Hello World");
+        //JOptionPane.showMessageDialog(null,"Hello World");
         int var = 1;
         Persona estudiante = new Persona();
-        estudiante.id = 1;
+        estudiante.setId(1);
         estudiante.nombre = "Jhon";
         estudiante.apellido = "Doe";
         estudiante.edad = 25;
-        System.out.println(estudiante.imprimir());
+        //System.out.println(estudiante.imprimir());
+        estudiante.saludar();
     }
 }
