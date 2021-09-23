580500dff4c90fa0eca4e17718a31349353461eb
AndoreiCR <dexter_andrey@hotmail.com>
2021-09-22 20:04:38 -0600

    Ultimos cambios
    Ejemplo de poliformismo

--- World.java
+++ World.java
@@ -4,14 +4,24 @@ import javax.swing.JPopupMenu;
 class HelloWorld {
 
     public static void main(String[] args) {
-        //JOptionPane.showMessageDialog(null,"Hello World");
+        // JOptionPane.showMessageDialog(null,"Hello World");
         int var = 1;
-        Persona estudiante = new Persona();
+        Estudiante estudiante = new Estudiante();
         estudiante.setId(1);
         estudiante.nombre = "Jhon";
         estudiante.apellido = "Doe";
         estudiante.edad = 25;
-        //System.out.println(estudiante.imprimir());
+        // System.out.println(estudiante.imprimir());
         estudiante.saludar();
+
+        Persona persona = new Persona();
+        persona.setId(3);
+
+        verIdPersona(persona);
+        verIdPersona(estudiante);
+    }
+
+    static void verIdPersona(Persona persona) {
+        System.out.println(persona.getId());
     }
 }
