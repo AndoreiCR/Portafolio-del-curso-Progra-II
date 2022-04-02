0542ddcdc2f9c3ecbad3dde49209706f83a0ba4b
AndoreiCR <dexter_andrey@hotmail.com>
2022-04-01 21:39:42 -0600

    prueba
    demo

--- Dexter_Varela_Tarea4/Ejercicio_2/src/Entities/AppPrueba.java
+++ Dexter_Varela_Tarea4/Ejercicio_2/src/Entities/AppPrueba.java
@@ -1,8 +1,9 @@
 package Entities;
-
+//esto es una prueba
 public class App {
     public static void main(String[] args) throws Exception {
         juego j = new juego();
         j.jugar();
+        System.out.print("prueba");
     }
 }
